5d2fa49347ef8c579b72d84240ff145188ca3a98
1831402 <1831402@LABCMAQ12.itp.local>
2019-09-30 10:43:38 -0300

    mudei aaaaaaas variaveis

--- biblioteca/acesso.php
+++ biblioteca/acesso.php
@@ -2,11 +2,11 @@
 
 define('ACESSO', true);
 
-function acessoLogar($usuario) {
-    if(!empty($usuario)) { //se o usuario não for vazio, logo existe o usuário na base com as credenciais
+function acessoLogar($usuarios) {
+    if(!empty($usuarios)) { //se o usuario não for vazio, logo existe o usuário na base com as credenciais
         $_SESSION["acesso"] = array( //cria a sessao acesso com os dados do usuario
-            "email" => $usuario["email"], 
-            "papel" => $usuario["papel"]
+            "email" => $usuarios["email"], 
+            "tipousuario" => $usuarios["tipousuario"]
         );
         return true; 
     }
@@ -24,9 +24,9 @@ function acessoUsuarioEstaLogado() {
     return isset($_SESSION["acesso"]);
 }
 
-function acessoPegarPapelDoUsuario() {
+function acessoPegarTipoDoUsuario() {
     if (acessoUsuarioEstaLogado()) {
-        return $_SESSION["acesso"]["papel"];
+        return $_SESSION["acesso"]["tipousuario"];
     }
 }
 
